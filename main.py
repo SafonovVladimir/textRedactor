83a517af848950ed642d01142b5e4ee4bf160fe6
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-29 14:53:46 +0300

    textRedactor

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ f_text.pack(fill=BOTH, expand=1)
 l_menu = Label(f_menu, text='Menu', bg='#2B3239', fg='#C6DEC1', font='Arial 10')
 l_menu.place(x=10, y=10)
 
-t = Text(f_text, bg='#343D46', fg='#C6DEC1', padx=10, pady=10, wrap=WORD, insertbackground='#EDA756')
-t.pack(fill=BOTH, expand=1)
+t = Text(f_text, bg='#343D46', fg='#C6DEC1', padx=10, pady=10, wrap=WORD,
+         insertbackground='#EDA756', selectbackground='#4E5A65', spacing1=1)
+t.pack(fill=BOTH, expand=1, side=LEFT)
 
-scrollbar = Scrollbar(t)
-scrollbar.pack(side=RIGHT, fill=Y)
-
+scrollbar = Scrollbar(f_text, command=t.yview)
+scrollbar.pack(side=LEFT, fill=Y)
+t.config(yscrollcommand=scrollbar.set)
 
 
 
