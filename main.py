dca1e387fc7bdc34dfe96011646432f88f8c7c54
Volodymyr Safonov <vladimir_safonov@ukr.net>
2022-03-30 11:49:17 +0300

    textEditor

--- main.py
+++ main.py
@@ -3,30 +3,35 @@ from tkinter import *
 root = Tk()
 root.geometry('600x400+400+150')
 
-# f_menu = Frame(root, bg='#1F252A', height=40)
-# f_menu.pack(fill=X)
+main_menu = Menu(root)
+root.config(menu=main_menu)
+
+
+def about_program():
+    print('About')
+
+
+# Menu File
+file_menu = Menu(main_menu, tearoff=0)
+file_menu.add_command(label='Create')
+file_menu.add_command(label='Open')
+file_menu.add_command(label='Save')
+file_menu.add_command(label='Save as...')
+file_menu.add_separator()
+file_menu.add_command(label='Quit')
+main_menu.add_cascade(label='File', menu=file_menu)
+
+# Menu About
+help_menu = Menu(main_menu, tearoff=0)
+help_menu_sub = Menu(help_menu, tearoff=0)
+help_menu_sub.add_command(label='Online')
+help_menu_sub.add_command(label='Offline')
+help_menu.add_cascade(label='Help', menu=help_menu_sub)
+help_menu.add_command(label='About', command=about_program)
+main_menu.add_cascade(label='About', menu=help_menu)
+
 f_text = Frame(root, bg='white')
 f_text.pack(fill=BOTH, expand=1)
-
-# l_menu = Label(f_menu, text='Menu', bg='#2B3239', fg='#C6DEC1', font='Arial 10')
-# l_menu.place(x=10, y=10)
-#
-#
-# def add_str():
-#     t.insert('2.4', 'Hello!')
-#
-#
-# def del_str():
-#     t.delete('1.0', END)
-#
-#
-# def get_str():
-#     print(t.get('1.0', END))
-#
-#
-# btn_add = Button(root, text='Add', command=add_str).place(x=50, y=10)
-# btn_del = Button(root, text='Delete', command=del_str).place(x=90, y=10)
-# btn_get = Button(root, text='Get', command=get_str).place(x=140, y=10)
 
 t = Text(f_text, bg='#343D46', fg='#C6DEC1', padx=10, pady=10, wrap=WORD,
          insertbackground='#EDA756', selectbackground='#4E5A65', spacing1=1, width=20)
